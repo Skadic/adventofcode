790a6fc3512e4e2f04286b49b4a4c833da63dd98
Etienne Palanga <e.palanga@disroot.org>
2022-12-17 00:59:49 +0100

    Finish C# day 1

--- 2022/CSharp/day01/Program.cs
+++ 2022/CSharp/day01/Program.cs
@@ -17,6 +17,12 @@
         static void Main(string[] args)
         {
             string input = File.ReadAllText("./input.txt");
+            Part1(input);
+            Part2(input);
+        }
+
+        static List<Elf> ProcessInput(string input)
+        {
             string[] elfStrings = input.Split("\n\n");
             List<Elf> elves = new List<Elf>();
 
@@ -35,10 +41,25 @@
                 elves.Add(elf);
             }
 
+            return elves;
+        }
+
+        static void Part1(string input)
+        {
+            var elves = ProcessInput(input);
             Elf maxElf = (from elf in elves
                           orderby elf.Calories.Sum() descending
                           select elf).First();
-            Console.WriteLine(maxElf.Calories.Sum());
+            Console.WriteLine($"Part 1: {maxElf.Calories.Sum()}");
+        }
+
+        static void Part2(string input)
+        {
+            var elves = ProcessInput(input);
+            var elfMax = (from elf in elves
+                          orderby elf.Calories.Sum() descending
+                          select elf).Take(3).Sum(elf => elf.Calories.Sum());
+            Console.WriteLine($"Part 2: {elfMax}");
         }
 
     }
